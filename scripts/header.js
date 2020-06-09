b8fe797bd271767b1cc65f216164ae0426b5511d
Abhik Ahuja <ahujaabhik@berkeley.edu>
2020-06-08 18:30:33 -0700

    Optimized navBar test

--- scripts/header.js
+++ scripts/header.js
@@ -15,33 +15,47 @@ $( window ).resize(function() {
   stickyOffset = $('.sticky').offset().top;
 });
 
-$(window).scroll(function() {
-  $("#landing").waypoint(function() {
-      // if (!typed) {
-      //     sleep(1000);
-      //     typeWriter("output", textArray);
-      // }
-      // typed = true;
-      $("#nav").children("ul").children(".active").removeClass("active");
-      $("#homeNav").addClass("active");
-  })
+$(document).ready(function() {
 
-  $("#software").waypoint(function() {
-      // if (!typed) {
-      //     sleep(1000);
-      //     typeWriter("output", textArray);
-      // }
-      // typed = true;
-      $("#nav").children("ul").children(".active").removeClass("active");
-      $("#softwareNav").addClass("active");
-  })
-  $("#experience").waypoint(function() {
-      // if (!typed) {
-      //     sleep(1000);
-      //     typeWriter("output", textArray);
-      // }
-      // typed = true;
-      $("#nav").children("ul").children(".active").removeClass("active");
-      $("#experienceNav").addClass("active");
-  })
-});
+  // $(window).scroll(function() {
+  //   console.log('nut');
+
+  //   $('.target').each(function() {
+  //     if($(window).scrollTop() >= $(this).offset().top) {
+  //         var id = $(this).attr('id');
+  //         $('nav a').removeClass('active');
+  //         $('nav a[href=#'+ id +']').addClass('actived');
+  //     }
+  //   });
+
+    // $("#landing").waypoint(function() {
+    //     // if (!typed) {
+    //     //     sleep(1000);
+    //     //     typeWriter("output", textArray);
+    //     // }
+    //     // typed = true;
+    //     $("#nav").children("ul").children(".active").removeClass("active");
+    //     $("#homeNav").addClass("active");
+    // })
+
+    // $("#software").waypoint(function() {
+    //     // if (!typed) {
+    //     //     sleep(1000);
+    //     //     typeWriter("output", textArray);
+    //     // }
+    //     // typed = true;
+    //     $("#nav").children("ul").children(".active").removeClass("active");
+    //     $("#softwareNav").addClass("active");
+    // })
+    // $("#experience").waypoint(function() {
+    //     // if (!typed) {
+    //     //     sleep(1000);
+    //     //     typeWriter("output", textArray);
+    //     // }
+    //     // typed = true;
+    //     $("#nav").children("ul").children(".active").removeClass("active");
+    //     $("#experienceNav").addClass("active");
+    // })
+  // });
+
+})
